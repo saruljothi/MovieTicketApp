4a0eddec13d736ca8cb208f3cf11279c88b822d4
saruljothi <saruljothi@mobiquityinc.com>
2020-01-28 13:56:51 +0100

    first skeleton of app

---
+++ movieReviewApp/src/test/java/com/mobiquity/movieReviewApp/MovieReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobiquity.movieReviewApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
