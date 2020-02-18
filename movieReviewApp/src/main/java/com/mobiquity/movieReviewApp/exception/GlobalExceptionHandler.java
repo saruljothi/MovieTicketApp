dd4d0734f24cda53fe978810f4db0424485cb421
Marko Stanoevich <mstanoevich@mobiquityinc.com>
2020-02-18 10:42:37 +0100

    Change UserController to return a list of messages instead of a single string

--- movieReviewApp/src/main/java/com/mobiquity/movieReviewApp/exception/GlobalExceptionHandler.java
+++ movieReviewApp/src/main/java/com/mobiquity/movieReviewApp/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.mobiquity.movieReviewApp.exception;
 
 import com.mobiquity.movieReviewApp.model.ResponseMovieApp;
+import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +12,7 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(value = {UserException.class})
   protected ResponseEntity<Object> userException(UserException ue) {
-    return new ResponseEntity<>(new ResponseMovieApp(ue.getLocalizedMessage()),
+    return new ResponseEntity<>(new ResponseMovieApp(Arrays.asList(ue.getLocalizedMessage())),
         HttpStatus.UNAUTHORIZED);
   }
 }
